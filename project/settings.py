7a4fa10068ce71c92d5ff72eb9dc60ca7707f2a8
Mohammed <apple@apples-MacBook-Air.local>
2021-11-05 11:47:45 +0100

    imported django_heroku

--- project/settings.py
+++ project/settings.py
@@ -125,4 +125,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / 'static'
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
